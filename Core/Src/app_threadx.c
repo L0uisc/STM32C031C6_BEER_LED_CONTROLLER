1fcb856911dc10707002aae032bf37dc5a381497
Louis Cloete <louisc363@gmail.com>
2025-09-13 21:53:14 +0200

    LED thread first draft working!

--- Core/Src/app_threadx.c
+++ Core/Src/app_threadx.c
@@ -55,21 +55,26 @@ typedef struct
   uint32_t next_wakeup;
   uint32_t next_phase;
   uint32_t phase_count;
-  led_phase_t *phase_sequence;
+  const led_phase_t *const phase_sequence;
 } led_cycle_t;
 /* USER CODE END PTD */
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
 #define NUM_LED_GROUPS 5
-#define LED_THREAD_PRIORITY (TX_MAX_PRIORITIES << 1)
+#define LED_THREAD_PRIORITY (TX_MAX_PRIORITIES >> 1)
 #define TICKS_PER_TIME_UNIT 25
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
 /* USER CODE BEGIN PM */
 #define SEQUENCE_LENGTH(s) (sizeof(s) / sizeof(led_phase_t))
-#define CYCLE_DEFINITION(s) { .next_phase = 0, .phase_count = SEQUENCE_LENGTH(s), .phase_sequence = s }
+#define CYCLE_DEFINITION(s) { \
+        .next_wakeup = 0, \
+        .next_phase = 0, \
+        .phase_count = SEQUENCE_LENGTH(s), \
+        .phase_sequence = s \
+}
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
@@ -77,22 +82,22 @@ TX_SEMAPHORE tx_app_semaphore;
 /* USER CODE BEGIN PV */
 TX_THREAD led_group_threads[NUM_LED_GROUPS];
 
-led_phase_t group_1_sequence[] = {
+const led_phase_t group_1_sequence[] = {
 		{ .duration = 1, .colour = RGB_LED_YELLOW }
 };
-led_phase_t group_2_sequence[] = {
+const led_phase_t group_2_sequence[] = {
 		{ .duration = 1, .colour = RGB_LED_WHITE }
 };
-led_phase_t group_3_sequence[] = {
+const led_phase_t group_3_sequence[] = {
 		{ .duration = 4, .colour = RGB_LED_WHITE },
 		{ .duration = 24, .colour = RGB_LED_OFF }
 };
-led_phase_t group_4_sequence[] = {
+const led_phase_t group_4_sequence[] = {
 		{ .duration = 4, .colour = RGB_LED_OFF },
 		{ .duration = 4, .colour = RGB_LED_WHITE },
 		{ .duration = 20, .colour = RGB_LED_OFF }
 };
-led_phase_t group_5_sequence[] = {
+const led_phase_t group_5_sequence[] = {
 		{ .duration = 8, .colour = RGB_LED_OFF },
 		{ .duration = 4, .colour = RGB_LED_WHITE },
 		{ .duration = 16, .colour = RGB_LED_OFF }
@@ -101,21 +106,21 @@ led_phase_t group_5_sequence[] = {
 led_cycle_t led_cycle[NUM_LED_GROUPS] = {
 		CYCLE_DEFINITION(group_1_sequence),
 		CYCLE_DEFINITION(group_2_sequence),
-		CYCLE_DEFINITION(group_1_sequence),
-		CYCLE_DEFINITION(group_2_sequence),
-		CYCLE_DEFINITION(group_3_sequence)
+		CYCLE_DEFINITION(group_3_sequence),
+		CYCLE_DEFINITION(group_4_sequence),
+		CYCLE_DEFINITION(group_5_sequence)
 };
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 /* USER CODE BEGIN PFP */
 VOID App_Led_Group_Function(ULONG data);
-led_phase_t *App_Led_Group_Sequence_Select(int index);
-GPIO_TypeDef *App_Led_Group_Get_GPIO_Port(int index);
-uint16_t App_Led_Group_Get_Pin_Mask(int index, led_colour_t colour, GPIO_PinState set_reset);
-uint16_t App_Led_Group_Get_Red_Pin_Mask(int index);
-uint16_t App_Led_Group_Get_Green_Pin_Mask(int index);
-uint16_t App_Led_Group_Get_Blue_Pin_Mask(int index);
+static inline const led_phase_t *const App_Led_Group_Sequence_Select(int index);
+static inline GPIO_TypeDef *App_Led_Group_Get_GPIO_Port(int index);
+static inline uint16_t App_Led_Group_Get_Pin_Mask(int index, led_colour_t colour, GPIO_PinState set_reset);
+static inline uint16_t App_Led_Group_Get_Red_Pin_Mask(int index);
+static inline uint16_t App_Led_Group_Get_Green_Pin_Mask(int index);
+static inline uint16_t App_Led_Group_Get_Blue_Pin_Mask(int index);
 /* USER CODE END PFP */
 
 /**
@@ -138,22 +143,22 @@ UINT App_ThreadX_Init(VOID *memory_ptr)
   }
 
   /* USER CODE BEGIN App_ThreadX_Init */
-  for (int i = 0; i < NUM_LED_GROUPS; i++)
+  for (int i = 0; i < NUM_LED_GROUPS; ++i)
   {
     void *buffer;
-    if ((ret = tx_byte_allocate(byte_pool, &buffer, TX_MINIMUM_STACK, TX_NO_WAIT)) != TX_SUCCESS)
+    if ((ret = tx_byte_allocate(byte_pool, &buffer, 300, TX_NO_WAIT)) != TX_SUCCESS)
     {
       goto err;
     }
 
     char name[9];
-    sprintf(name, "Group %d", i);
+    sprintf(name, "Group %d", i + 1);
     if ((ret = tx_thread_create(&led_group_threads[i],
     		name,
 			App_Led_Group_Function,
-			0,
+			i,
 			buffer,
-			TX_MINIMUM_STACK,
+			300,
 			LED_THREAD_PRIORITY,
 			LED_THREAD_PRIORITY,
 			TX_NO_TIME_SLICE,
@@ -161,6 +166,13 @@ UINT App_ThreadX_Init(VOID *memory_ptr)
     {
       goto err;
     }
+  }
+
+  tx_time_set(0);
+
+  for (int i = 0; i < NUM_LED_GROUPS; ++i)
+  {
+    tx_semaphore_ceiling_put(&tx_app_semaphore, NUM_LED_GROUPS);
   }
 
 err:
@@ -190,11 +202,13 @@ void MX_ThreadX_Init(void)
 /* USER CODE BEGIN 1 */
 VOID App_Led_Group_Function(ULONG data)
 {
+  tx_semaphore_get(&tx_app_semaphore, TX_WAIT_FOREVER);
+
   for (;;)
   {
     led_cycle_t *cycle = &led_cycle[data];
     int phase_idx = cycle->next_phase;
-    led_phase_t *next_phase = &cycle->phase_sequence[phase_idx];
+    const led_phase_t *const next_phase = &cycle->phase_sequence[phase_idx];
     cycle->next_wakeup += next_phase->duration * TICKS_PER_TIME_UNIT;
     cycle->next_phase = (phase_idx + 1) % cycle->phase_count;
 
@@ -208,7 +222,7 @@ VOID App_Led_Group_Function(ULONG data)
   }
 }
 
-led_phase_t *App_Led_Group_Sequence_Select(int index)
+static inline const led_phase_t *const App_Led_Group_Sequence_Select(int index)
 {
   switch (index + 1)
   {
@@ -227,7 +241,7 @@ led_phase_t *App_Led_Group_Sequence_Select(int index)
   }
 }
 
-GPIO_TypeDef *App_Led_Group_Get_GPIO_Port(int index)
+static inline GPIO_TypeDef *App_Led_Group_Get_GPIO_Port(int index)
 {
   switch (index + 1)
   {
@@ -241,20 +255,32 @@ GPIO_TypeDef *App_Led_Group_Get_GPIO_Port(int index)
     return GROUP4_R_GPIO_Port;
   case 5:
     return GROUP5_R_GPIO_Port;
+  case 6:
+    return GROUP5_R_GPIO_Port;
+  case 7:
+    return GROUP5_R_GPIO_Port;
+  case 8:
+    return GROUP5_R_GPIO_Port;
+  case 9:
+    return GROUP5_R_GPIO_Port;
+  case 10:
+    return GROUP5_R_GPIO_Port;
   default:
     return NULL;
   }
 }
 
-uint16_t App_Led_Group_Get_Pin_Mask(int index, led_colour_t colour, GPIO_PinState set_reset)
+static inline uint16_t App_Led_Group_Get_Pin_Mask(int index, led_colour_t colour, GPIO_PinState set_reset)
 {
   uint16_t mask = 0;
-  if (colour & RGB_LED_RED == set_reset) mask |= App_Led_Group_Get_Red_Pin_Mask(index);
-  if (colour & RGB_LED_GREEN == set_reset) mask |= App_Led_Group_Get_Green_Pin_Mask(index);
-  if (colour & RGB_LED_BLUE == set_reset) mask |= App_Led_Group_Get_Blue_Pin_Mask(index);
+  if ((colour & RGB_LED_RED) == (set_reset ? RGB_LED_RED : 0)) mask |= App_Led_Group_Get_Red_Pin_Mask(index);
+  if ((colour & RGB_LED_GREEN) == (set_reset ? RGB_LED_GREEN : 0)) mask |= App_Led_Group_Get_Green_Pin_Mask(index);
+  if ((colour & RGB_LED_BLUE) == (set_reset ? RGB_LED_BLUE : 0)) mask |= App_Led_Group_Get_Blue_Pin_Mask(index);
+
+  return mask;
 }
 
-uint16_t App_Led_Group_Get_Red_Pin_Mask(int index)
+static inline uint16_t App_Led_Group_Get_Red_Pin_Mask(int index)
 {
   switch (index + 1)
   {
@@ -268,12 +294,22 @@ uint16_t App_Led_Group_Get_Red_Pin_Mask(int index)
     return GROUP4_R_Pin;
   case 5:
     return GROUP5_R_Pin;
+  case 6:
+    return GROUP6_R_Pin;
+  case 7:
+    return GROUP7_R_Pin;
+  case 8:
+    return GROUP8_R_Pin;
+  case 9:
+    return GROUP9_R_Pin;
+  case 10:
+    return GROUP10_R_Pin;
   default:
     return 0;
   }
 }
 
-uint16_t App_Led_Group_Get_Green_Pin_Mask(int index)
+static inline uint16_t App_Led_Group_Get_Green_Pin_Mask(int index)
 {
   switch (index + 1)
   {
@@ -287,12 +323,22 @@ uint16_t App_Led_Group_Get_Green_Pin_Mask(int index)
     return GROUP4_G_Pin;
   case 5:
     return GROUP5_G_Pin;
+  case 6:
+    return GROUP6_G_Pin;
+  case 7:
+    return GROUP7_G_Pin;
+  case 8:
+    return GROUP8_G_Pin;
+  case 9:
+    return GROUP9_G_Pin;
+  case 10:
+    return GROUP10_G_Pin;
   default:
     return 0;
   }
 }
 
-uint16_t App_Led_Group_Get_Blue_Pin_Mask(int index)
+static inline uint16_t App_Led_Group_Get_Blue_Pin_Mask(int index)
 {
   switch (index + 1)
   {
@@ -306,6 +352,16 @@ uint16_t App_Led_Group_Get_Blue_Pin_Mask(int index)
     return GROUP4_B_Pin;
   case 5:
     return GROUP5_B_Pin;
+  case 6:
+    return GROUP6_B_Pin;
+  case 7:
+    return GROUP7_B_Pin;
+  case 8:
+    return GROUP8_B_Pin;
+  case 9:
+    return GROUP9_B_Pin;
+  case 10:
+    return GROUP10_B_Pin;
   default:
     return 0;
   }
